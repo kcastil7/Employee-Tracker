a08efafedc1cbf83aef4f692a28f5eaf3d5b0161
Kevin Castillo <kcas51@yahoo.com>
2022-05-14 19:10:46 -0700

    finsished project

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+const { execPath } = require("process");
 const db = require("./db/connection");
 
 
@@ -39,15 +40,14 @@ async function mainMenu() {
 mainMenu();
 
 async function search(ans) {
-    console.log("You are searching for an " + ans.Choice.split(' ')[2]);
-    const [rows, fields] = await db.query("SELECT * FROM " + ans.Choice.split(' ')[2])
+    const [rows, fields] = await db.query("SELECT * FROM " + ans.Choice.split(' ')[2]);
     // if (err) {
     //     console.log(err);
     //     return;
     // }
     console.table(rows);
 
-    console.log("--------------------------")
+    console.log("--------------------------");
     mainMenu();
 
 }
@@ -55,15 +55,17 @@ async function search(ans) {
 async function add(ans) {
     console.log("You are adding a " + ans.Choice.split(' ')[2]);
     if(ans.Choice.split(' ')[2] == "department"){
-        await inquirer.prompt([
+         await inquirer.prompt([
             {
               type: "input",
               name: "new_department",
               message: "What is the name of the department you'd like to add?",
             },
           ])
-        .then(function (response) {
-            console.log(response);
+        .then(async function (response) {
+            const sql = `INSERT INTO departments (name) VALUES (?)`;
+            const [rows,fields] = await db.query(sql,response.new_department)
+            console.log("New Department added");
         });
     }
     else if(ans.Choice.split(' ')[2] == "role"){
@@ -84,8 +86,10 @@ async function add(ans) {
               message: "Which department does this role belong in?",
             },
           ])
-        .then(function (response){
+        .then(async function (response){
             console.log(response);
+            const sql = `INSERT INTO roles (title,salary,department_id) SELECT ?,?, id FROM departments WHERE name = ? `;
+            const [rows,fields] = await db.query(sql,[response.new_role,response.role_salary,response.role_department]);
         });
     }
     else if(ans.Choice.split(' ')[2] == "employee"){   
@@ -112,8 +116,17 @@ async function add(ans) {
                 "Enter the name of the employee's manager. If this employee doesn't have a manager, leave it blank and press enter.",
             },
           ])
-        .then(function(response) {
-            console.log(response);
+        .then(async function(response) {
+          if(!response.employee_manager || response.employee_manager === ""){
+            const sql = `INSERT INTO employees (first_name,last_name,role_id) SELECT ?,?, roles.id FROM roles WHERE title = ?;`;
+            const [rows,fields] = await db.query(sql,[response.employee_first,response.employee_last,response.employee_role]);
+          }
+          else {
+            const sql = `INSERT INTO employees (first_name,last_name,role_id,manager_id) SELECT ?,?, roles.id, manager.id FROM roles, employees manager WHERE roles.title = ? AND manager.first_name = ? AND manager.last_name = ?;`;
+            const [rows,fields] = await db.query(sql,[response.employee_first,response.employee_last,response.employee_role,response.employee_manager.split(' ')[0],response.employee_manager.split(' ')[1]])
+          }
+
+
         });
 
     }
@@ -124,5 +137,44 @@ async function add(ans) {
 
 async function update(ans) {
     console.log("You are updating " + ans.Choice.split(' ')[1] + " " + ans.Choice.split(' ')[2]);
+    const [rows,fields] = await db.query(`SELECT first_name, last_name FROM employees;`);
+    
+    let names = [];
+    rows.forEach( data => {
+      names.push(data.first_name + " " + data.last_name);
+    });
+    let roles = [];
+    const [rows1,fields1] = await db.query(`SELECT * FROM roles;`)
+    rows1.forEach(data => {
+      const temp = {
+        name:data.title,
+        value: data.id
+      }
+      roles.push(temp);
+    })
+    console.log(roles);
+    await inquirer.prompt(
+      [
+        {
+          type: "list",
+          name: "employees",
+          message: "Choose the employee you'd like to update",
+          choices: names,
+        },
+        {
+          type: "list",
+          name: "roles",
+          message: "Which role would you like to assign this employee to?",
+          choices: roles,
+        },
+      ]
+    )
+    .then(async function(response) {
+      console.log(response);
+      const sql = `UPDATE employees SET role_id = ? WHERE first_name = ? AND last_name = ?`
+      const [rows,fields] = await db.query(sql,[response.roles,response.employees.split(' ')[0],response.employees.split(' ')[1]])
+    });
     mainMenu();
+
+    
 }
